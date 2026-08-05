0231b550eb42587e98078e93f9c7b35f47c9764b
durgaprasad0809 <durgaprasad.a0809@gmail.com>
2026-08-05 23:21:04 +0530

    Created the backend laod balancer

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z06489251KBRFEKHGTQPC"
+}  
+
+variable "domain_name" {
+    default = "daws90s-durga.shop"
+}
