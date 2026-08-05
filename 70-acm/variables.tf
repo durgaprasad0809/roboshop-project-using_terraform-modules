8c9a3c5504740edd0611149f57f847c3bc3cf220
durgaprasad0809 <durgaprasad.a0809@gmail.com>
2026-08-05 23:27:51 +0530

    created the SSL HTTPS certificate usin using terraform code

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws90s-durga.shop"
+}
+
+variable "zone_id" {
+    default = "Z06489251KBRFEKHGTQPC"
+}  
